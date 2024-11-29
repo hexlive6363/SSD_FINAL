d87b52e5b172dd77c2154d869e2fba61d3c2c166
Sanchit Kumar <kumarsanchit19114@gmail.com>
2024-11-29 21:46:06 +0530

    Added Background

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def home():
     return render_template('after.html', data=prediction)
 
 if __name__ == "__main__":
-    app.run(host='10.42.0.198', port=5000)
+    app.run(host='127.0.0.1', port=5000)
